c25bb8cad0469471a1a825ff33d919721531b0d5
Ismayil Ahmadli <38913625+Ismayil92@users.noreply.github.com>
2020-06-16 01:14:42 +0200

    Update matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -12,7 +12,7 @@ class Matrix{
       {
           
       }
-      Matrix(const std::vector<std::vector<int> > &in_array):rows(in_array.size()), cols{in_array[0].size()}
+      Matrix(const std::vector<std::vector<int>> &in_array):rows(in_array.size()), cols{in_array[0].size()}
       { 
         
           for (int i=0; i<rows;i++)
@@ -20,7 +20,7 @@ class Matrix{
             array_int.push_back(in_array[i]);
           }
       }
-      void scalarMultiplication(const int &c)
+      void scalarMultiplication(const int c)
       {
           for (int i=0; i<rows;i++)
           {
@@ -44,7 +44,7 @@ class Matrix{
         }
         else
         {
-            std::cout<<"Vector dimensions are not equal"<<std::endl;
+            std::cout<<"Dimensions are not equal"<<std::endl;
         }
       }
       void setArray(const std::vector<std::vector<int> > &input_matrix)
@@ -64,7 +64,7 @@ class Matrix{
       }
       std::vector<std::vector<int>> getArray()
       {
-            return array_int;
+          return array_int;
       }
       int getRows()
       {
@@ -150,13 +150,36 @@ class Vector: public Matrix
         }
         else
         {
-            std::cout<<"Vector dimensions are not equal"<<std::endl;
+            std::cout<<"Dimensions are not equal"<<std::endl;
         }
     }
-    
+    void setArray(const std::vector<std::vector<int> > &input_matrix)
+    {  
+          rows = input_matrix.size();
+          cols = input_matrix[0].size();
+          std::vector<int> array_row;
+          for (int i=0; i<rows;i++)
+          { 
+              
+              for(int j=0; j<cols; j++)
+              {
+                  array_row.push_back(input_matrix[i][j]);
+              }
+              array_int.push_back(array_row);
+              array_row.clear();
+          }
+    }
     int getVectorLength()
     {
         return vec_length;
+    }
+    int getRows()
+    {
+        return rows; 
+    }
+    int getCols()
+    {
+        return cols;
     }
     void display()
     {
@@ -178,6 +201,38 @@ class Vector: public Matrix
           
     }
 };
+
+//Dot product 
+Vector dot(Matrix m1, Vector v1)
+{   
+    int scalar = 0;
+    std::vector<std::vector<int>> m1_array = m1.getArray();
+    std::vector<std::vector<int>> v1_array = v1.getArray();
+    std::vector<int> vec_row;
+    std::vector<std::vector<int>> vec;
+    Vector vec_dot_result;
+    if(m1.getCols() == v1.getRows())
+    {
+        for(int i=0; i<m1.getRows(); i++)
+        {
+            for(int j=0; j<m1.getCols(); j++)
+            {
+                scalar += m1_array[i][j]*v1_array[j][0];
+            }
+            
+            vec_row.push_back(scalar);
+            vec.push_back(vec_row);
+            vec_row.clear();
+        }
+        vec_dot_result.setArray(vec);
+        
+    }
+    else
+    {
+        std::cout<<"Matrix and vector dimensions are not suitable"<<std::endl;
+    }
+    return vec_dot_result; 
+}
 // Sum two different matrixes 
 Matrix sum(Matrix m1, Matrix m2)
 {
@@ -191,7 +246,7 @@ Matrix sum(Matrix m1, Matrix m2)
     
     if(m1.getRows()==m2.getRows() && m1.getCols()==m2.getCols())
     {   
-        std::cout<<"Matrix dimensions are the same"<<std::endl;
+        std::cout<<" Matrix dimensions are the same"<<std::endl;
         std::vector<int> matrix_row;
         for(int i=0; i<rows; i++)
         {
@@ -202,7 +257,6 @@ Matrix sum(Matrix m1, Matrix m2)
             }
             sum_mat.push_back(matrix_row);
             matrix_row.clear();
-            
         }
         m3.setArray(sum_mat);
         
@@ -214,44 +268,58 @@ Matrix sum(Matrix m1, Matrix m2)
     return m3;
 }
 
-/* Sum function for 2 different vectors
-Vector sum2vec(Vector v1, Vector v2)
-{
-   Vector v3;
-   std::vector<int> v1_vec = v1.getVector();
-   std::vector<int> v2_vec = v2.getVector();
-   std::vector<int> new_vec;
-   
-   if(v1.getVectorLength() == v2.getVectorLength())
-   {
-       for(int i=0; i<v1.getVectorLength(); i++)
-       {
-           new_vec.push_back(v1_vec[i]+v2_vec[i]);
-       }
-       v3.setVector(new_vec);
-   }
-   else
-   {
-       std::cout<<"Vector dimensions are not the same"<<std::endl;
-   }
-   return v3;
-}
-*/
+
 int main()
 { 
-  std::vector<std::vector<int> > a = {{5,4},{1,4}};
-  Matrix arr1(a);
+  std::cout<<"-------- Vector operations --------"<<"\n";
+  std::cout<<" Vector a: "<<"\n";
+  std::vector<std::vector<int>> a = {{5,1}};
+  Vector arr1(a);
+  arr1.display();
+  std::cout<<" Vector b: "<<"\n";
+  std::vector<std::vector<int>> b = {{1,2}};
+  Vector arr2(b);
+  arr2.display();
+  // Vector scalar multiplication
+  std::cout<<"---- Scalar multiplication of the vector a by 3 ----"<<std::endl;
   arr1.scalarMultiplication(3);
   arr1.display();
-  std::vector<std::vector<int> > b = {{1,4},{2,4}};
-  Matrix arr2(b);
-  arr2.display();
+  // Vector sum
+  std::cout<<"---- Sum of two vector after scalar multiplication ----"<<std::endl;
   arr1.sum(arr2);
   arr1.display();
-  
-  Matrix new_matrix = sum(arr1,arr2);
-  new_matrix.display();
 
+  std::cout<<"-------- Matrix operations --------"<<"\n";
+  std::cout<<" Matrix a: "<<"\n";
+  std::vector<std::vector<int>> a_1 = {{5,1,3},{3,4,5}};
+  Matrix mat1(a_1);
+  mat1.display();
+  std::cout<<" Matrix b: "<<"\n";
+  std::vector<std::vector<int>> b_1 = {{1,2,4},{4,6,5}};
+  Matrix mat2(b_1);
+  mat2.display();
+  // Matrix scalar multiplication
+  std::cout<<"---- Scalar multiplication of the matrix a by 3 ----"<<std::endl;
+  mat1.scalarMultiplication(3);
+  mat1.display();
+  // Matrix sum 
+  std::cout<<"------- Matrix sum -------"<<std::endl;
+  Matrix new_mat = sum(mat1,mat2);
+  new_mat.display();
+  std::cout<<"------ Dot product -----------"<<std::endl;
+  std::cout<<" Case: Dimensions are suitable "<<std::endl;
+  std::vector<std::vector<int>> dot_mat1_array = {{5,4},{1,4}};
+  Matrix dot_mat1(dot_mat1_array);
+  std::vector<std::vector<int>> dot_vec1_array = {{15},{4}};
+  Vector dot_vec1(dot_vec1_array);
+  Vector new_vec1 = dot(dot_mat1,dot_vec1);
+  new_vec1.display();
+  std::cout<<" Case: Dimensions are not suitable "<<std::endl;
+  std::vector<std::vector<int>> dot_mat2_array = {{10,4},{1,10}};
+  Matrix dot_mat2(dot_mat2_array);
+  std::vector<std::vector<int>> dot_vec2_array = {{1,2}};
+  Vector dot_vec2(dot_vec2_array);
+  Vector new_vec2 = dot(dot_mat2,dot_vec2);
+  new_vec2.display();
   return 0;
 }  
-
